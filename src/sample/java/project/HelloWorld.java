55702db7ea42d1fc021bd5d9d664f8a7e51cce3c
anshida-m <anshida@litmus7.com>
2022-11-02 15:06:18 +0530

    commit(2) -HeloWorld.java

--- src/sample/java/project/HelloWorld.java
+++ src/sample/java/project/HelloWorld.java
@@ -2,6 +2,11 @@ package sample.java.project;
 
 class HelloWorld{  
     public static void main(String args[]){  
-     System.out.println("Hello World...");  
+     System.out.println("Hello World...");
+     aMethod();
+    }
+
+    public static void aMethod(){
+	    System.out.println("You are awesome!");
     }
 }
